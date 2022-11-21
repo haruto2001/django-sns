28a54b4997de81bbcf2bdf1541e10c006e91263a
haruto2001 <tarako5515@gmail.com>
2022-11-21 16:21:47 +0900

    feat: register an user model to admin site

--- accounts/models.py
+++ accounts/models.py
@@ -112,7 +112,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['email', 'nickname']
 
     class Meta:
-        verbose_name = _('ユーザ')
-        verbose_name_plural = _('ユーザ群')
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
 
