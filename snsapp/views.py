982ad881c14b1da25db756ad3909a4b9d9361912
haruto2001 <tarako5515@gmail.com>
2022-11-22 17:18:06 +0900

    feat: create views of my and others' posts

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class Home(LoginRequiredMixin, ListView):
+   """HOMEページで、自分以外のユーザー投稿をリスト表示"""
+   model = Post
+   template_name = 'base.html'
+
+   def get_queryset(self):
+       #リクエストユーザーのみ除外
+       return Post.objects.exclude(user=self.request.user)
+
+
+class MyPost(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'base.html'
+
+    def get_queryset(self):
+        #自分の投稿に限定
+        return Post.objects.filter(user=self.request.user)
