5aa0f869c31a1ee19e534dffa359211aea023109
haruto2001 <tarako5515@gmail.com>
2022-11-22 17:23:54 +0900

    fix: fix typos

--- snsapp/views.py
+++ snsapp/views.py
@@ -6,7 +6,7 @@ from .models import Post
 class Home(LoginRequiredMixin, ListView):
    """HOMEページで、自分以外のユーザー投稿をリスト表示"""
    model = Post
-   template_name = 'base.html'
+   template_name = 'list.html'
 
    def get_queryset(self):
        #リクエストユーザーのみ除外
@@ -15,7 +15,7 @@ class Home(LoginRequiredMixin, ListView):
 
 class MyPost(LoginRequiredMixin, ListView):
     model = Post
-    template_name = 'base.html'
+    template_name = 'list.html'
 
     def get_queryset(self):
         #自分の投稿に限定
