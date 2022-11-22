0c435ce882022fe8cfad9b88587daac3861628b5
haruto2001 <tarako5515@gmail.com>
2022-11-22 17:12:26 +0900

    feat: create post model

--- snsapp/models.py
+++ snsapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    image = models.ImageField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_at"]  # 投稿順にクエリを取得
