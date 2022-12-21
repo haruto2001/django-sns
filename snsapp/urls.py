e56763170be6fda91475025ee37bf7d355f1eb60
haruto2001 <tarako5515@gmail.com>
2022-12-21 18:55:17 +0900

    feat: add like-xxx to urls in order to display Likes on home and detail pages

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, MyPost, DetailPost, UpdatePost, CreatePost, DeletePost
+from .views import Home, MyPost, CreatePost, DetailPost, UpdatePost, DeletePost, LikeHome, LikeDetail
 
 
 app_name = 'snsapp'
@@ -7,8 +7,10 @@ app_name = 'snsapp'
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('mypost/', MyPost.as_view(), name='mypost'),
+    path('create/', CreatePost.as_view(), name='create'),
     path('detail/<int:pk>', DetailPost.as_view(), name='detail'),
     path('detail/<int:pk>/update', UpdatePost.as_view(), name='update'),
     path('detail/<int:pk>/delete', DeletePost.as_view(), name='delete'),
-    path('create/', CreatePost.as_view(), name='create'),
+    path('like-home/<int:pk>', LikeHome.as_view(), name='like-home'),
+    path('like-detail/<int:pk>', LikeDetail.as_view(), name='like-detail'),
 ]
